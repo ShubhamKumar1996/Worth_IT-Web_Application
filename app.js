4376e99390bb911ba2d9025bd4dc2335b390f6af
ShubhamKumar <nimcetshubhamkumar@gmail.com>
2020-06-30 20:08:54 +0530

    Adding Simple Middlewares

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const express = require("express");  // Importing express Module.
+// Inbuild Packages.
 
+// End of Inbuilt Packages.
+
+// Third Party Packages.
+
+const express = require("express");  // Importing express Module.
 const app = express();  //Initializing object which will give us access to various functionality.
 
+
+// End Of Third Party Packages.
+
+// Importing User Defined Modules.
+
+// End of User Defined Modules.
+
+// Here We write all the Middlewares.
+
+app.use((req, res, next)=>{
+    console.log("First MiddleWare");
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("Second MiddleWare");
+    next();
+});
+
+app.use((req, res, next)=>{
+    res.send("<h1>Page Not Found</h1>");
+});
+
+// End of MiddleWares.
+
+// Creating Server
 app.listen(8080); // creating server with port 8080.
