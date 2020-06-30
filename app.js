1b7562d481cab02c17114a27aa3c937cec757287
ShubhamKumar <nimcetshubhamkumar@gmail.com>
2020-06-30 18:20:25 +0530

    Created Simple server

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+const express = require("express");  // Importing express Module.
+
+const app = express();  //Initializing object which will give us access to various functionality.
+
+app.listen(8080); // creating server with port 8080.
