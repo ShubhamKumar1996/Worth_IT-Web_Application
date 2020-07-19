eb28ffa03f9f9b9fa6c829168d1951fe98a59a65
ShubhamKumar <nimcetshubhamkumar@gmail.com>
2020-07-19 18:26:01 +0530

    Serving dynamic content, Using ejs Template engine

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ const bodyParser = require("body-parser");
 // End Of Third Party Packages.
 //________________________________________________________
 
+// Setting globals declarations
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// End of global declarations
+//____________________________________________________
+
 // Importing User Defined Modules.
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 // End of User Defined Modules.
 //_________________________________________________________
@@ -27,11 +35,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({extended:false}));  
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{                         // Page Not Found Middleware.
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.render("404.ejs", {
+        pageTitle: "Page Not Found",
+        path: ""
+    });
 });
 // End of MiddleWares.
 //_________________________________________________________
