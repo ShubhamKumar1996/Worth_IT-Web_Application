a2c40d4940b883b850dba6771d010733c7a4b5e1
ShubhamKumar <nimcetshubhamkumar@gmail.com>
2020-09-01 19:02:55 +0530

    Added Mailing on SignUp

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
@@ -69,11 +77,17 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
-        });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup Succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      });
 };
 
 exports.postLogout = (req, res, next) => {
